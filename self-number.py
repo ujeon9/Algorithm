a2a0d596cff18d641ffddae15e0756db34d7d85c
YOUJEONG LEE <34884287+ujeon9@users.noreply.github.com>
2019-07-19 13:18:58 +0900

    Update self-number.py

--- self-number.py
+++ self-number.py
@@ -4,9 +4,5 @@
 # If number has not any generator, it called self-number
 # {} : sets
 # Find the sum of the self-number from 1 to 5000
+
 print(sum(set(range(1, 5000)) - {n + sum((int(i) for i in str(n))) for n in range(1, 5000)}))
-
-
-
-
-
