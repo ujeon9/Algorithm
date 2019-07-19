3deeed7d81adc8033646b92df9cdc33b595a36be
YOUJEONG LEE <34884287+ujeon9@users.noreply.github.com>
2019-07-19 11:45:27 +0900

    Add files via upload

---
+++ self-number.py
@@ -0,0 +1,12 @@
+# d(91)=9+1+91=101
+# n : d(n)'s generator
+# 91, 10 : 101's generator
+# If number has not any generator, it called self-number
+# {} : sets
+
+print(sum(set(range(1, 5000)) - {n + sum((int(i) for i in str(n))) for n in range(1, 5000)}))
+
+
+
+
+
